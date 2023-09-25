c5f89ff4511f70a9dd6b8c87551123a86123ecde
Javier <javierlogronoaguirre@gmail.com>
2023-09-25 10:58:35 +0200

    clean code

--- db_connect.js
+++ db_connect.js
@@ -4,9 +4,7 @@ const mongoose = require ('mongoose')
 const config = require('./config')
 
 const conndbaccounts = mongoose.createConnection(config.dbaccounts, { useMongoClient: true })
-const conndbdata = mongoose.createConnection(config.dbdata, { useMongoClient: true })
 
 module.exports = {
-	conndbaccounts,
-	conndbdata
+	conndbaccounts
 }
